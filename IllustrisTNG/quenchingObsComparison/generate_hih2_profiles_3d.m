3999a07744dabfba8d8806dbf05ab7a3066c9200
eladzing <64072621+eladzing@users.noreply.github.com>
2021-07-15 14:40:51 +0300

    Update generate_hih2_profiles_3d.m
    added a normlized offset

--- IllustrisTNG/quenchingObsComparison/generate_hih2_profiles_3d.m
+++ IllustrisTNG/quenchingObsComparison/generate_hih2_profiles_3d.m
@@ -135,13 +135,20 @@ for j=1:4
                 ms=cgmH_star.xMedian;
                 hm=cgmH_star.yMedian;
             end
+            ms=ms(~isnan(ms));
+            hm=hm(~isnan(ms));
             msMass=interp1(ms,hm,log10(gMass(mask)));
             
             hiProfD=mk_meanMedian_bin(radPosition(mask),(hiMass(jj,mask)-10.^msMass),'bins',binEdges);
+            hiProfDN=mk_meanMedian_bin(radPosition(mask),hiMass(jj,mask)./(10.^msMass)-1,'bins',binEdges);
+
             %h2ProfN=mk_meanMedian_bin(radPosition(mask),h2Mass(jj,mask)./gMass(mask),'bins',binEdges);
             
             byHost.(comps{k}).hiMassAvgD(jj,:,j)=hiProfD.yMean;
             byHost.(comps{k}).hiMassMedD(jj,:,j)=hiProfD.yMedian;
+            
+             byHost.(comps{k}).hiMassAvgDN(jj,:,j)=hiProfDN.yMean;
+            byHost.(comps{k}).hiMassMedDN(jj,:,j)=hiProfDN.yMedian;
             
             %byHost.(comps{k}).h2MassAvgN(jj,:,j)=h2ProfN.yMean;
             %byHost.(comps{k}).h2MassMedN(jj,:,j)=h2ProfN.yMedian;
@@ -192,6 +199,30 @@ for j=1:3
             
             byMass.(comps{k}).h2MassAvgN(jj,:,j)=h2ProfN.yMean;
             byMass.(comps{k}).h2MassMedN(jj,:,j)=h2ProfN.yMedian;
+            
+            
+             %%hi offset
+            if strcmp(comps{k},'Gal')
+                ms=galH_star.xMedian;
+                hm=galH_star.yMedian;
+            elseif strcmp(comps{k},'CGMall')
+                ms=cgmH_star.xMedian;
+                hm=cgmH_star.yMedian;
+            end
+            ms=ms(~isnan(ms));
+            hm=hm(~isnan(ms));
+            msMass=interp1(ms,hm,log10(gMass(mask)));
+            
+            hiProfD=mk_meanMedian_bin(radPosition(mask),(hiMass(jj,mask)-10.^msMass),'bins',binEdges);
+            hiProfDN=mk_meanMedian_bin(radPosition(mask),hiMass(jj,mask)./(10.^msMass)-1,'bins',binEdges);
+            %h2ProfN=mk_meanMedian_bin(radPosition(mask),h2Mass(jj,mask)./gMass(mask),'bins',binEdges);
+            
+            byMass.(comps{k}).hiMassAvgD(jj,:,j)=hiProfD.yMean;
+            byMass.(comps{k}).hiMassMedD(jj,:,j)=hiProfD.yMedian;
+            
+            byMass.(comps{k}).hiMassAvgDN(jj,:,j)=hiProfDN.yMean;
+            byMass.(comps{k}).hiMassMedDN(jj,:,j)=hiProfDN.yMedian;
+            
         end
     end
 end

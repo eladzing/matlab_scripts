63c1ab0e8345830489151722dd2541176e778aaf
eladzing <64072621+eladzing@users.noreply.github.com>
2021-07-23 15:24:35 +0300

    Update acquire_gal_properties.m
    updating for new properties

--- IllustrisTNG/jellyfish/acquire_gal_properties.m
+++ IllustrisTNG/jellyfish/acquire_gal_properties.m
@@ -11,22 +11,26 @@ ngal=length(objectTable.subfind);
 galProps.stellarMass=zeros(1,ngal);
 galProps.hostM200c=zeros(1,ngal);
 galProps.hostR200c=zeros(1,ngal);
+galProps.hostM200m=zeros(1,ngal);
+galProps.hostR200m=zeros(1,ngal);
 galProps.galStellarMass=zeros(1,ngal);
 galProps.gasMass=zeros(1,ngal);
 galProps.galGasMass=zeros(1,ngal);
 galProps.galSFR=zeros(1,ngal);
-%galProps.pos=zeros(3,ngal);
-%galProps.vel=zeros(3,ngal);
-% galProps.rpos=zeros(1,ngal);
-% galProps.vrad=zeros(1,ngal);
-% galProps.vtan=zeros(1,ngal);
+galProps.pos=zeros(3,ngal);
+galProps.vel=zeros(3,ngal);
+galProps.rpos=zeros(1,ngal);
+galProps.vrad=zeros(1,ngal);
+galProps.vtan=zeros(1,ngal);
 %galProps.nSatHost=zeros(1,ngal);
 %galProps.rhoLocal=zeros(size(objectTable.subfind));
 %galProps.ramPress=zeros(size(objectTable.subfind));
 %galProps.mach=zeros(size(objectTable.subfind));
 
-
-for i=1:length(snaps)
+for k=1:length(sims)
+    
+    
+    for i=1:length(snaps)
     
     snap=snaps(i);
     

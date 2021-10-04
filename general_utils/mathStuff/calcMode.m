099544f3be095ca37ec0c27c070ed48e8b56dccc
eladzing <64072621+eladzing@users.noreply.github.com>
2021-10-04 08:49:26 +0300

    Create calcMode.m
    a function to calculate the mode (the most frequent value) of of a distribution of non-integers. The result is usually dependant on the binning.

---
+++ general_utils/mathStuff/calcMode.m
@@ -0,0 +1,24 @@
+function res = calcMode(arr,nbins)
+%CALCMODE _calculate the mode of non-integer distributions by constructing
+%a histogram
+%   for a given array of values build a histogram and identify the most
+%   frequent value
+
+%nbinsDef=20;
+
+if ~exist('nbins','var')
+    [nc,edges]=histcounts(arr);
+else
+     [nc,edges]=histcounts(arr,nbins);
+end
+
+
+[~,ix]=max(nc);
+
+res=0.5.*(sum(edges(ix:ix+1)));
+
+
+
+
+end
+

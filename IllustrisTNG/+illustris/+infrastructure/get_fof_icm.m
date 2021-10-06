c339ebbf3bfcc303463e9d77679f8cf333249231
eladzing <64072621+eladzing@users.noreply.github.com>
2021-10-06 19:17:14 +0300

    Update get_fof_icm.m
    fixed issue with subhalos with no gas cells.

--- IllustrisTNG/+illustris/+infrastructure/get_fof_icm.m
+++ IllustrisTNG/+illustris/+infrastructure/get_fof_icm.m
@@ -10,14 +10,19 @@ function outMask = get_fof_icm(fofs,id,snap)
 
 global BASEPATH
 groupOffset = illustris.snapshot.getSnapOffsets(BASEPATH,snap,id,'Group');% get the relevant particle indices
+
+if groupOffset.lenType(1)
+    fprintf('FoF ID %i has no gas elements \n',current_function().upper,id);
+    return
+end
+
 groupFirstInd=groupOffset.offsetType(1)+1;
 %groupLastInd=groupFirstInd+int64(groupOffset.lenType(1))-1;
 
 outMask=true(1,groupOffset.lenType(1));
 
 %% test
-fofGas=illustris.snapshot.loadHalo(BASEPATH, snap, id, 'gas','ParticleIDs');
-
+fofGas=illustris.snapshot.loadHalo(BASEPATH, snap, id, 'gas',{'ParticleIDs'});
 
 %% generate a list of subhalos
 subIDs=fofs.GroupFirstSub(id+1) + (0:fofs.GroupNsubs(id+1)-1);
@@ -27,6 +32,10 @@ for i=2:length(subIDs)
     
     
     subOffset= illustris.snapshot.getSnapOffsets(BASEPATH,snap,subIDs(i),'Subhalo');% get the relevant particle indices
+    if subOffset.lenType(1)==0 % skip if subhalo has no gas 
+        continue
+    end
+    
     subFirstInd=subOffset.offsetType(1)+1;
     subLastInd=subFirstInd+int64(subOffset.lenType(1))-1;
     
@@ -37,7 +46,7 @@ for i=2:length(subIDs)
     
     %% test
     try
-        satGas=illustris.snapshot.loadSubhalo(BASEPATH, snap, subIDs(i), 'gas','ParticleIDs');
+        satGas=illustris.snapshot.loadSubhalo(BASEPATH, snap, subIDs(i), 'gas',{'ParticleIDs'});
         if any(satGas~=fofGas(inds))
             error('particle ids do not match up')
         end

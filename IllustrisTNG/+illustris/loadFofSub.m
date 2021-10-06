2a55ac82c61308460a5fc1ff75f46fcc3d7c0172
eladzing <64072621+eladzing@users.noreply.github.com>
2021-10-06 15:44:23 +0300

    Update loadFofSub.m
    fixed bug  - need to ise the basepath when in a function

--- IllustrisTNG/+illustris/loadFofSub.m
+++ IllustrisTNG/+illustris/loadFofSub.m
@@ -5,6 +5,7 @@ function [subs,fofs,subsInfo]=loadFofSub(snap)
 global DEFAULT_MATFILE_DIR
 global simDisplayName
 global DRACOFLAG
+global BASEPATH
 
 if ~exist('snap','var')
     fprintf('snap not defined, defaulting to snap=99 \n')
@@ -14,8 +15,8 @@ end
 if ~DRACOFLAG
     load([ DEFAULT_MATFILE_DIR '/fofs_subs_' simDisplayName '_snp' num2str(snap) '.mat'])
 else
-    fofs=illustris.groupcat.loadHalos(bp,snap);
-    subs=illustris.groupcat.loadSubhalos(bp,snap);
+    fofs=illustris.groupcat.loadHalos(BASEPATH,snap);
+    subs=illustris.groupcat.loadSubhalos(BASEPATH,snap);
 end
 
 subsInfo = illustris.infrastructure.build_sub_fof_connection(subs,fofs);

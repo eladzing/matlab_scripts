9de34752c72a92035221b4b5c29f00f019a0b632
eladzing <64072621+eladzing@users.noreply.github.com>
2021-10-19 15:11:55 +0300

    Update calc_ssfr.m
    added an option to get the ssfr in the entire subhalo,. The default is still the stellar mass within the galactic radius - 2 X stellar half-mass radius.

--- IllustrisTNG/+illustris/+utils/calc_ssfr.m
+++ IllustrisTNG/+illustris/+utils/calc_ssfr.m
@@ -2,10 +2,11 @@ function ssfr = calc_ssfr(subStruct,varargin)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-global illUnits 
+global illUnits
 
 base=10^0.5*1e-17;
 scat=0.5;
+type='gal'; % sSFR is calculated within galactic radius (2*stellar half mass)
 
 i=1;
 while i<=length(varargin)
@@ -16,21 +17,31 @@ while i<=length(varargin)
         case {'scatter','scat','fuzz'}
             i=i+1;
             scat=varargin{i};
+        case {'full','sub'}
+            type='full'; % sSFR is calculated from entire subhalo
+        case {'gal','galaxy'}
+            type='gal'; % sSFR is calculated within galactic radius (2*stellar half mass)
+            
         otherwise
             error('CALC_SSFR - illegal argument: %s',varargin{i});
     end
     i=i+1;
 end
 
-  
-stellarMass= subStruct.SubhaloMassInRadType(illustris.partTypeNum('stars')+1,:).*illUnits.massUnit; % stellar mass within 2*rhalf
-sfr= subStruct.SubhaloSFRinRad;
+switch type
+    case 'gal'
+        stellarMass= subStruct.SubhaloMassInRadType(illustris.partTypeNum('stars')+1,:).*illUnits.massUnit; % stellar mass within 2*rhalf
+        sfr= subStruct.SubhaloSFRinRad;
+    case 'full'
+        stellarMass= subStruct.SubhaloMassType(illustris.partTypeNum('stars')+1,:).*illUnits.massUnit; % stellar mass within 2*rhalf
+        sfr= subStruct.SubhaloSFR;
+end
 
 ssfr=sfr./stellarMass; % + base.*10.^(scat.*rand(size(sfr)));
 
- mask=ssfr==0;
- 
- ssfr(mask)=base.*10.^(scat.*rand(1,sum(mask)));
+mask=ssfr==0;
+
+ssfr(mask)=base.*10.^(scat.*rand(1,sum(mask)));
 
 end
 

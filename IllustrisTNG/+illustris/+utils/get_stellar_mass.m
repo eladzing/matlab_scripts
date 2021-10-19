36dd86592d773288ce80c1a2b044a51e4ab70239
eladzing <64072621+eladzing@users.noreply.github.com>
2021-10-19 15:11:31 +0300

    Update get_stellar_mass.m
    added an option to get the stellar mass in the entire subhalo, or only in the CGM. The default is still the stellar mass within the galactic radius - 2 X stellar half-mass radius.

--- IllustrisTNG/+illustris/+utils/get_stellar_mass.m
+++ IllustrisTNG/+illustris/+utils/get_stellar_mass.m
@@ -1,13 +1,36 @@
-function res = get_stellar_mass(subs,mask)
-%GET_STELLAR_MASS - get the galaxy stellar mass for TNG gal's, i.e. 
-%    stellar mass within 2*rhalf. 
+function res = get_stellar_mass(subs,varargin) %mask,type)
+%GET_STELLAR_MASS - get the galaxy stellar mass for TNG gal's, i.e.
+%    stellar mass within 2*rhalf.
 
-if ~exist('mask','var')
-    mask=true(1,subs.count);
+type='gal'; % stellar mass within galactic radius
+mask=true(1,subs.count);
+
+i=1;
+while i<=length(varargin)
+    switch lower(varargin{i})
+        case 'mask'
+            i=i+1;
+            mask=varargin{i};
+        case {'full','sub'}
+            type='full';  % all stellar mass in the subhalo
+        case {'gal','galaxy'}
+            type='gal';
+        case 'cgm'  % all stellar mass in the subhalo, beyond the galactic radius
+            type='cgm';
+    end
+    i=i+1;
 end
 
-global illUnits
-res = double(subs.SubhaloMassInRadType(illustris.partTypeNum('stars')+1,mask).*illUnits.massUnit); 
 
+global illUnits
+switch type
+    case 'gal'
+        res = double(subs.SubhaloMassInRadType(illustris.partTypeNum('stars')+1,mask).*illUnits.massUnit);
+    case 'full'
+        res = double(subs.SubhaloMassType(illustris.partTypeNum('stars')+1,mask).*illUnits.massUnit);
+    case 'cgm='
+        res = (double(subs.SubhaloMassType(illustris.partTypeNum('stars')+1,mask))-...
+            double(subs.SubhaloMassInRadType(illustris.partTypeNum('stars')+1,mask))).*illUnits.massUnit;
+        
 end
 

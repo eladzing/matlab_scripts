246027b001767196beb7508d1fdf079e40e61d55
eladzing <64072621+eladzing@users.noreply.github.com>
2021-11-30 15:13:25 +0200

    Update prepare_jellyfish_catalog_CJF.m
    since I"m adding the funky galaxies to the object table, we no longer need to account for them in the catalog

--- IllustrisTNG/jellyfish/prepare_jellyfish_catalog_CJF.m
+++ IllustrisTNG/jellyfish/prepare_jellyfish_catalog_CJF.m
@@ -6,27 +6,27 @@ load([DEFAULT_MATFILE_DIR '/cosmic_jellyfish_objectTable.mat'])
 %load([DEFAULT_MATFILE_DIR '/jellyfishScores_TNG50.mat'])
 
 %% funky galaxies:
-funkyTNG50.snap33=[21560, 50698, 90630, 127581, 138654];
-funkyTNG50.snap40= [102, 126, 158, 36250, 66110, 112204, 163074];
-funkyTNG50.snap50=79623;
-funkyTNG50.snap59=[];
-funkyTNG50.snap67=356638;
-funkyTNG50.snap72=91931;
-funkyTNG50.snap78=[226474, 451164];
-funkyTNG50.snap84= [];
-funkyTNG50.snap91= [];
-funkyTNG50.snap99= [];
-
-funkyTNG100.snap33=[];
-funkyTNG100.snap40= [];
-funkyTNG100.snap50=10011;
-funkyTNG100.snap59=[301337, 301338];
-funkyTNG100.snap67= [];
-funkyTNG100.snap72= [];
-funkyTNG100.snap78= [];
-funkyTNG100.snap84= [];
-funkyTNG100.snap91= [];
-funkyTNG100.snap99= [];
+% funkyTNG50.snap33=[21560, 50698, 90630, 127581, 138654];
+% funkyTNG50.snap40= [102, 126, 158, 36250, 66110, 112204, 163074];
+% funkyTNG50.snap50=79623;
+% funkyTNG50.snap59=[];
+% funkyTNG50.snap67=356638;
+% funkyTNG50.snap72=91931;
+% funkyTNG50.snap78=[226474, 451164];
+% funkyTNG50.snap84= [];
+% funkyTNG50.snap91= [];
+% funkyTNG50.snap99= [];
+% 
+% funkyTNG100.snap33=[];
+% funkyTNG100.snap40= [];
+% funkyTNG100.snap50=10011;
+% funkyTNG100.snap59=[301337, 301338];
+% funkyTNG100.snap67= [];
+% funkyTNG100.snap72= [];
+% funkyTNG100.snap78= [];
+% funkyTNG100.snap84= [];
+% funkyTNG100.snap91= [];
+% funkyTNG100.snap99= [];
 %% Generate catalogs by simulations and snapshots snapshots based on an object Table
 
 
@@ -34,19 +34,19 @@ snaps=unique(objectTable.snap);
 sims=unique(objectTable.sim);
 
 % run over simulations
-for k=2:length(sims)
+for k=1:length(sims)
     bp=illustris.set_env(sims(k),'nomount');
     global simDisplayName
     fprintf('running on simulation %s \n ', sims(k));
     
     simMask=strcmp(objectTable.sim,sims(k));
     
-    switch sims(k)
-        case 'TNG100'
-            funky=funkyTNG100;
-        case 'TNG50'
-            funky=funkyTNG50;
-    end
+%     switch sims(k)
+%         case 'TNG100'
+%             funky=funkyTNG100;
+%         case 'TNG50'
+%             funky=funkyTNG50;
+%     end
             
     
     % run over snap
@@ -64,7 +64,13 @@ for k=2:length(sims)
         
         % find the objects from the right snapshot
         snapMask=objectTable.snap==snaps(i);
+        
         fullMask=snapMask & simMask;
+        
+        if ~any(fullMask)  % skip this snap if there are no objects in the sim/snap
+            continue
+        end
+        
         tabInd=find(fullMask); % indices in objectTable from the right simulation and snpashot
         
         % find their subfind index in the table
@@ -75,14 +81,14 @@ for k=2:length(sims)
         galNcls(subfindInd)=objectTable.clsNum(tabInd);
         galScoreTot(subfindInd)=objectTable.scoreTotal(tabInd);
         
-        %% address funky galaxyis 
-        funkyID=funky.(['snap' num2str(snaps(i))]);
-        
-        if ~isempty(funkyID)
-            galScore(funkyID+1)=0;
-            galNcls(funkyID+1)=0;
-            galScoreTot(funkyID+1)=0;
-        end
+%         %% address funky galaxyis 
+%         funkyID=funky.(['snap' num2str(snaps(i))]);
+%         
+%         if ~isempty(funkyID)
+%             galScore(funkyID+1)=0;
+%             galNcls(funkyID+1)=0;
+%             galScoreTot(funkyID+1)=0;
+%         end
         
         outStruct.Score=galScore;
         outStruct.ClassificationNum=galNcls;

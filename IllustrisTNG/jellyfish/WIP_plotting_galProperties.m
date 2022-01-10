d3b8c09ebb2da62944371327a8a094df464851e5
eladzing <64072621+eladzing@users.noreply.github.com>
2022-01-10 16:09:40 +0200

    Update WIP_plotting_galProperties.m
    This file may need to be removed...

--- IllustrisTNG/jellyfish/WIP_plotting_galProperties.m
+++ IllustrisTNG/jellyfish/WIP_plotting_galProperties.m
@@ -9,8 +9,6 @@ if ~exist('cax','var')
     cax=[min(cc) max(cc)];
 end
 
-figure
-set(gcf,'position',[1432 421 1000 750],'Color','w')
 
 
 %% 
@@ -18,10 +16,18 @@ set(gcf,'position',[1432 421 1000 750],'Color','w')
 xx=log10(galProps.galStellarMass);
 yy=log10(galProps.hostM200c);
 
-[bird, binsize, xl,yl]= histogram2d(xx(mask100 & ~maskJF),yy(mask100 & ~maskJF),ones(size(xx(mask100))),'len',50);
+xl0=[min(xx(mask100)) max(xx(mask100))].*[0.99 1.01];
+yl0=[min(yy(mask100)) max(yy(mask100))].*[0.99 1.01];
+%% prepare data for plotting  
+[bird, binsize, xl,yl]= histogram2d(xx(mask100 & ~maskJF),yy(mask100 & ~maskJF),...
+ones(size(xx(mask100))),'xlim',xl0,'ylim',yl0,'len',50);
 
 filt=fspecial('disk',15);
 popCont=plot_population_contour(xx(maskJF & mask100),yy(maskJF & mask100),'smooth',filt,'noplot');
+
+jfscore=ones( size(xx(mask100)));
+jfscore(~maskJF)=0;
+
 
 %% identify points beyond contour
 xxx=xx(mask100 & maskJF);
@@ -37,27 +43,53 @@ end
 
 
 cmap=brewermap(256,'Reds');
+cmap=inferno(256);
 qCol=brewermap(8,'Set1');
+
+jfCol=[0.1,0.1,0.89];
+njfCol=[0.89,0.1,0.11];
+cols=cat(1,njfCol,jfCol);
 %% background with full sample grayscale
 figure
+figure('position',[1432 421 1000 750],'Color','w')
 
-%ax0=axes;
+%% underlying hist 
+
+hh=scatterhist(xx(mask100),yy(mask100),'Group',jfscore(mask100),...
+    'location','northeast','direction','out','legend','off','color',cols,...
+    'markersize',1);
+
+%0.6950    0.6960
+set(hh(1),'position',[0.1000    0.1000   0.7 0.7 ],'fontsize',14);
+hh(1).YLabel.String='';
+hh(1).XLabel.String='';
+    %'XTick',[],'YTick',[],'YAxisLocation','left','XAxisLocation','bottom');
+set(hh(2),'position',[0.80 0.1 0.119806451612903 0.75]);
+set(hh(3),'position',[0.1 0.80 0.75 0.132374233128834]);
+hh(2).Children(1).LineWidth=1.5;
+hh(2).Children(2).LineWidth=1.5;
+hh(3).Children(1).LineWidth=1.5;
+hh(3).Children(2).LineWidth=1.5;
 xlim(xl);ylim(yl);
 
+%% map & contout 
+ax1=axes;
+pos=get(hh(1),'position');
+set(ax1,'position',pos);
 
 imagesc(xl,yl,squeeze(bird(:,:,1)))
-set(gca,'ydir','normal')
+set(gca,'ydir','normal','fontsize',14)
 
 colormap(cmap)
 
 %% JF
 
-%ax1=axes;
+
 hold on
 %qCol=[0.89,0.1,0.11];
 sCol=[0.1,0.1,0.89];
 [~,h(1)]=contour(popCont.xx,popCont.yy,popCont.popContour,'ShowText','off','LineColor',[0 0 1],...
- 'LineColor',sCol,'linewidth',1.8,...
+ 'LineColor',sCol,'linewidth',2,...
     'LevelList',[98 75:-25:5],'Fill','off','linestyle','-',...
     'DisplayName','Jellyfish');    
 
@@ -65,7 +97,21 @@ sCol=[0.1,0.1,0.89];
 plot(xxx(score>98),yyy(score>98),'^','color',sCol,'markersize',6.5,'linewidth',1.5,'MarkerFaceColor',qCol(2,:),...
     'Displayname','JF beyond 98 percentile');
 
-legend(h)
+%legend(h)
+
+xfac=0.83; yfac=0.08;
+text(xfac.*diff(xl)+xl(1),yfac.*diff(yl)+yl(1),'TNG100',...%'Edgecolor','k','backgroundcolor',[1,0.97,0.97],...
+    'Interpreter','latex','fontsize',17,'fontweight','bold','color','k')
+
+
 
 xlim(xl);ylim(yl);
-xlabelmine
+
+xlabelmine('log Stellar Mass',16)
+ylabelmine('log host $M_\mathrm{200}$',16)
+
+
+linkaxes([hh(1),ax1])  %linkaxes([ax0,ax1,ax2])
+%ax2.Visible = 'off';ax2.XTick = [];ax2.YTick = [];
+%ax1.Visible = 'off';ax1.XTick = [];ax1.YTick = [];
+%hh(1).Visible = 'off';hh(1).XTick = [];hh(1).YTick = [];

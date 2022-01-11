aed63c56b662ec133d9b1af05df2af45cb4c0e67
eladzing <64072621+eladzing@users.noreply.github.com>
2022-01-11 18:07:07 +0200

    Create plotsSFRProf.m
    creates the plots for HI deficiency vs ssfr in average and median approach and in stellar mass and host mass slicing

---
+++ IllustrisTNG/quenchingObsComparison/plotsSFRProf.m
@@ -0,0 +1,274 @@
+
+function plotsSFRProf(profStr,type,sim,model)
+
+switch(lower(sim))
+    case 'tng50'
+        legendPanel=1;
+    case 'tng100'
+        legendPanel=2;
+end
+
+figPos=[ 800          42        1062         954];
+colors=brewermap(8,'Set1');
+cind=[2 5 3 1];
+htag={'11-12','12-13','13-14','14-15'};
+mtag={'8.3-9','9-10','10-11','11-12'};
+modelTag={'BR', 'GK', 'KMT'};
+xl=log10([1e-13 5e-9]);
+
+%% in Stellar Mass Bins 
+
+hf=figure('color','w','position',figPos);
+
+t=tiledlayout(2,2);
+h=[];
+
+switch(lower(type))
+    case 'gal'
+        yl=[-0.4 0.25];
+    case 'cgmall'
+        yl=[-2 3];
+end
+
+
+for k=1:4
+    nexttile;
+    hflag=true;
+    h=[];
+    for j=1:4
+        
+        profSt=profStr.byHostStar4(k).(type).hiProfDSFRN(model,j);
+        msk=profSt.binCount>0;
+        if any(msk) && sum(profSt.binCount)>10
+            
+            px=[profSt.xMedian(msk) fliplr(profSt.xMedian(msk))];
+            py=[profSt.yQuarts(2,msk) fliplr(profSt.yQuarts(3,msk))];
+            
+            patch(px,py,colors(cind(j),:),'facealpha',0.35,'edgecolor','none')
+            if hflag;hold on; hflag=false;end
+            h(end+1)=plot(profSt.xMedian(msk),profSt.yMedian(msk),'-',...
+                'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',htag{j} );
+        end
+        
+    end
+    
+    if k==legendPanel
+        legend(h,'fontsize',14,'location','southeast','interpreter','latex');
+    end
+    
+    
+    xlim(xl);
+    ylim(yl);
+    xl=xlim;
+    yl=ylim;
+    
+    grid
+    text(xl(1)+0.05.*diff(xl),yl(1)+0.95.*diff(yl),...
+        ['$ M_\mathrm{*} =' mtag{k} '$'],'interpreter','latex',...
+        'fontsize',16);
+    
+    set(gca,'fontsize',14,'box','on')
+    
+    
+    xlabelmine('$r/R_\mathrm{vir}$',16);
+    ylabelmine('HI deficiency',16);
+    
+    
+end
+
+t.TileSpacing = 'compact';
+
+sgtitle([sim ' ' type ' ' modelTag{model} ' - Median and 25-75 percentile'],...
+    'Interpreter','latex','fontsize',18);
+%% Average
+
+hf=figure('color','w','position',figPos);
+
+h=[];
+%yl=[-0.4 0.25];
+%xl=[0 2.4];
+
+t=tiledlayout(2,2);
+
+for k=1:4
+    nexttile;
+    hflag=true;
+    h=[];
+    for j=1:4
+        
+        profSt=profStr.byHostStar4(k).(type).hiProfDSFRN(model,j);
+        msk=profSt.binCount>0;
+        if any(msk) && sum(profSt.binCount)>10
+            
+            px=[profSt.xMean(msk) fliplr(profSt.xMean(msk))];
+            py=[profSt.yMean(msk)-0.5.*profSt.yStanDev(msk), ...
+                fliplr(profSt.yMean(msk)+0.5.*profSt.yStanDev(msk))];
+            patch(px,py,colors(cind(j),:),'facealpha',0.35,'edgecolor','none')
+            if hflag;hold on; hflag=false;end
+            h(end+1)=plot(profSt.xMean(msk),profSt.yMean(msk),'-',...
+                'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',htag{j} );
+            %          h(end+1)=errorbar(profSt.xMean,profSt.yMean,profSt.yStanDev./2,'-',...
+            %             'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',htag{j} );
+            %
+            
+            %         h(end+1)=plot(hprofs50.byHostStar4(k).xMed(:,j),hprofs50.byHostStar4(k).Gal.hiMassMedD(1,:,j),'-',...
+            %             'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',htag{j} );
+        end
+        
+    end
+    
+    if  k==legendPanel
+        legend(h,'fontsize',14,'location','southeast','interpreter','latex');
+    end
+    
+    
+    xlim(xl);
+    ylim(yl);
+    xl=xlim;
+    yl=ylim;
+    
+    grid
+    text(xl(1)+0.05.*diff(xl),yl(1)+0.95.*diff(yl),...
+        ['$ M_\mathrm{*} =' mtag{k} '$'],'interpreter','latex',...
+        'fontsize',16);
+    
+    set(gca,'fontsize',14,'box','on')
+    xlabelmine('$r/R_\mathrm{vir}$',16);
+    ylabelmine('HI deficiency',16);
+    
+    
+end
+t.TileSpacing = 'compact';
+
+sgtitle([sim ' ' type ' ' modelTag{model} '- Mean and StdDev'],...
+    'Interpreter','latex','fontsize',18);
+
+
+
+
+%% in Host Mass Bins 
+legendPanel=3;
+hf=figure('color','w','position',figPos);
+
+t=tiledlayout(2,2);
+h=[];
+
+switch(lower(type))
+    case 'gal'
+        yl=[-0.4 0.25];
+    case 'cgmall'
+        yl=[-2 3];
+end
+
+%xl=[0 2.4];
+for k=1:4
+    nexttile;
+    hflag=true;
+    h=[];
+    for j=1:4
+        
+        profSt=profStr.byHostStar4(j).(type).hiProfDSFRN(model,k);
+        msk=profSt.binCount>0;
+        if any(msk) && sum(profSt.binCount)>10
+            
+            px=[profSt.xMedian(msk) fliplr(profSt.xMedian(msk))];
+            py=[profSt.yQuarts(2,msk) fliplr(profSt.yQuarts(3,msk))];
+            
+            patch(px,py,colors(cind(j),:),'facealpha',0.35,'edgecolor','none')
+            if hflag;hold on; hflag=false;end
+            h(end+1)=plot(profSt.xMedian(msk),profSt.yMedian(msk),'-',...
+                'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',mtag{j} );
+        end
+        
+    end
+    
+    if k==legendPanel
+        legend(h,'fontsize',14,'location','southeast','interpreter','latex');
+    end
+    
+    
+    xlim(xl);
+    ylim(yl);
+    xl=xlim;
+    yl=ylim;
+    
+    grid
+    text(xl(1)+0.05.*diff(xl),yl(1)+0.95.*diff(yl),...
+        ['$ M_\mathrm{*} =' htag{k} '$'],'interpreter','latex',...
+        'fontsize',16);
+    
+    set(gca,'fontsize',14,'box','on')
+    
+    
+    xlabelmine('$r/R_\mathrm{vir}$',16);
+    ylabelmine('HI deficiency',16);
+    
+    
+end
+
+t.TileSpacing = 'compact';
+
+sgtitle([sim ' ' type ' ' modelTag{model} ' - Median and 25-75 percentile'],...
+    'Interpreter','latex','fontsize',18);
+%% Average
+
+hf=figure('color','w','position',figPos);
+
+h=[];
+%yl=[-0.4 0.25];
+%xl=[0 2.4];
+
+t=tiledlayout(2,2);
+
+for k=1:4
+    nexttile;
+    hflag=true;
+    h=[];
+    for j=1:4
+        
+        profSt=profStr.byHostStar4(j).(type).hiProfDSFRN(model,k);
+        msk=profSt.binCount>0;
+        if any(msk) && sum(profSt.binCount)>10
+            
+            px=[profSt.xMean(msk) fliplr(profSt.xMean(msk))];
+            py=[profSt.yMean(msk)-0.5.*profSt.yStanDev(msk), ...
+                fliplr(profSt.yMean(msk)+0.5.*profSt.yStanDev(msk))];
+            patch(px,py,colors(cind(j),:),'facealpha',0.35,'edgecolor','none')
+            if hflag;hold on; hflag=false;end
+            h(end+1)=plot(profSt.xMean(msk),profSt.yMean(msk),'-',...
+                'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',mtag{j} );
+            %          h(end+1)=errorbar(profSt.xMean,profSt.yMean,profSt.yStanDev./2,'-',...
+            %             'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',htag{j} );
+            %
+            
+            %         h(end+1)=plot(hprofs50.byHostStar4(k).xMed(:,j),hprofs50.byHostStar4(k).Gal.hiMassMedD(1,:,j),'-',...
+            %             'linewidth',1.2,'color',colors(cind(j),:),'DisplayName',htag{j} );
+        end
+        
+    end
+    
+    if  k==legendPanel
+        legend(h,'fontsize',14,'location','southeast','interpreter','latex');
+    end
+    
+    
+    xlim(xl);
+    ylim(yl);
+    xl=xlim;
+    yl=ylim;
+    
+    grid
+    text(xl(1)+0.05.*diff(xl),yl(1)+0.95.*diff(yl),...
+        ['$ M_\mathrm{*} =' htag{k} '$'],'interpreter','latex',...
+        'fontsize',16);
+    
+    set(gca,'fontsize',14,'box','on')
+    xlabelmine('$r/R_\mathrm{vir}$',16);
+    ylabelmine('HI deficiency',16);
+    
+    
+end
+t.TileSpacing = 'compact';
+
+sgtitle([sim ' ' type ' ' modelTag{model} '- Mean and StdDev'],...
+    'Interpreter','latex','fontsize',18);

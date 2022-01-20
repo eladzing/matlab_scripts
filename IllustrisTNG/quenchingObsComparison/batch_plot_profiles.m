e0c694b80f5a7cbb70bb81f2fe780902f4a8bed9
eladzing <64072621+eladzing@users.noreply.github.com>
2022-01-20 14:35:10 +0200

    Create batch_plot_profiles.m
    batch file for plotting a bunch of profiles for different simulations and components

---
+++ IllustrisTNG/quenchingObsComparison/batch_plot_profiles.m
@@ -0,0 +1,15 @@
+%% batch plots 
+
+
+%% radius 
+plotRadProf(hprofs50,'Gal','TNG50',1)
+plotRadProf(hprofs50,'CGMall','TNG50',1)
+plotRadProf(hprofs100,'Gal','TNG100',1)
+plotRadProf(hprofs100,'CGMall','TNG100',1)
+
+
+%ssfr 
+plotsSFRProf(hprofs50,'Gal','TNG50',1)
+plotsSFRProf(hprofs50,'CGMall','TNG50',1)
+plotsSFRProf(hprofs100,'Gal','TNG100',1)
+plotsSFRProf(hprofs100,'CGMall','TNG100',1)

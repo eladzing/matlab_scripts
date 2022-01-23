eb2721191b7a0f9a448123552775543ef0692ee9
eladzing <64072621+eladzing@users.noreply.github.com>
2022-01-23 16:42:29 +0200

    Create generate_hostTag.m
    generate a unique tag to identify hosts. added as a field to the galaxy properties structure

---
+++ IllustrisTNG/jellyfish/+jellyfish/generate_hostTag.m
@@ -0,0 +1,9 @@
+function galPropStr = generate_hostTag(galPropStr)
+% GENERATE_HOSTTAG Add a host tag field tothe galaxy property structure 
+%   for a unique identifier of hosts across sims and snaps
+
+for j=1:length(galPropStr.hostID)
+    galPropStr.hostTag(j)=join([galPropStr.sim(j),"snp0",string(num2str(galPropStr.snap(j))), ...
+        "hostID",string(num2str(galPropStr.hostID(j)))],'');
+end
+

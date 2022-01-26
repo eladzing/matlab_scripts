d782027208eb5659c61547a16cbbb5240a93b1c4
eladzing <64072621+eladzing@users.noreply.github.com>
2022-01-26 17:14:16 +0200

    Update batch_plot_profiles.m
    added printing instructions

--- IllustrisTNG/quenchingObsComparison/batch_plot_profiles.m
+++ IllustrisTNG/quenchingObsComparison/batch_plot_profiles.m
@@ -2,14 +2,19 @@
 
 
 %% radius 
-plotRadProf(hprofs50,'Gal','TNG50',1)
-plotRadProf(hprofs50,'CGMall','TNG50',1)
-plotRadProf(hprofs100,'Gal','TNG100',1)
-plotRadProf(hprofs100,'CGMall','TNG100',1)
+
+plotRadProf(hprofs50,'CGMall','TNG50',1,'print')
+plotRadProf(hprofs100,'Gal','TNG100',1,'print')
+plotRadProf(hprofs100,'CGMall','TNG100',1,'print')
+close all
 
 
 %ssfr 
-plotsSFRProf(hprofs50,'Gal','TNG50',1)
-plotsSFRProf(hprofs50,'CGMall','TNG50',1)
-plotsSFRProf(hprofs100,'Gal','TNG100',1)
-plotsSFRProf(hprofs100,'CGMall','TNG100',1)
+plotsSFRProf(hprofs50,'Gal','TNG50',1,'print')
+plotsSFRProf(hprofs50,'CGMall','TNG50',1,'print')
+plotsSFRProf(hprofs100,'Gal','TNG100',1,'print')
+plotsSFRProf(hprofs100,'CGMall','TNG100',1,'print')
+close all
+
+plotRadProf_massN(hprofs50,'Gal','TNG50',1,'print')
+plotRadProf_massN(hprofs50,'CGMall','TNG50',1,'print')

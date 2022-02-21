f1e563e9ee3efd36477259dc52152ceddb97de6b
eladzing <64072621+eladzing@users.noreply.github.com>
2022-02-21 23:36:19 +0200

    Update mk_meanMedian_bin.m
    added a measure of standard deviation but w.r.t to the median, not the mean.

--- general_utils/dataAnalysis/mk_meanMedian_bin.m
+++ general_utils/dataAnalysis/mk_meanMedian_bin.m
@@ -49,10 +49,12 @@ nBins=length(bins);
 
 %% find mean and median in bins
 binYMean=zeros(1,nBins);
-binYStd=zeros(1,nBins);
+binYStd=zeros(1,nBins); % standard STD 
+binYStdMed=zeros(1,nBins); % STD with respect to the mean 
 binYQuantile=zeros(5,nBins);
 binXMean=zeros(1,nBins);
 binXStd=zeros(1,nBins);
+binXStdMed=zeros(1,nBins);
 binXQuantile=zeros(5,nBins);
 binCount=zeros(1,nBins);
 
@@ -60,16 +62,21 @@ binInd=discretize(xParam,binEdges);
 nanMask=~isnan(yParam);
 for i=1:nBins
     mask=binInd==i & nanMask;
+    binCount(i)=sum(mask);
+    
     
     binYMean(i)=mean(yParam(mask));
     binYStd(i)=std(yParam(mask));
     binYQuantile(:,i)=quantile(yParam(mask),[0.1 0.25 0.5 0.75 0.9]);
     
+    binYStdMed(i)=sqrt( sum((yParam(mask)-binYQuantile(3,i)).^2)./binCount(i));
+    
+    
     binXMean(i)=mean(xParam(mask));
     binXStd(i)=std(xParam(mask));
     binXQuantile(:,i)=quantile(xParam(mask),[0.1 0.25 0.5 0.75 0.9]);
+    binXStdMed(i)=sqrt( sum((xParam(mask)-binXQuantile(3,i)).^2)./binCount(i));
     
-    binCount(i)=sum(mask);
 end
 
 res.bins=bins;
@@ -78,11 +85,13 @@ res.binCount=binCount;
 
 res.yMean=binYMean;
 res.yStanDev=binYStd;
+res.yStanDevMed=binYStdMed;
 res.yMedian=binYQuantile(3,:);
 res.yQuarts=binYQuantile([1,2,4,5],:);
 
 res.xMean=binXMean;
 res.xStanDev=binXStd;
+res.xStanDevMed=binXStdMed;
 res.xMedian=binXQuantile(3,:);
 res.xQuarts=binXQuantile([1,2,4,5],:);
 res.quantiles=[0.1 0.25 0.75 0.9];

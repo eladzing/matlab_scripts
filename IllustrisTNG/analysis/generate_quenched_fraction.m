88af4a4bf3a29df58a937510cfbbe03198b6afb7
eladzing <64072621+eladzing@users.noreply.github.com>
2022-02-24 21:49:30 +0200

    Update generate_quenched_fraction.m
    continued work on function for generating quenched fractions. added bootstraping to create scatter

--- IllustrisTNG/analysis/generate_quenched_fraction.m
+++ IllustrisTNG/analysis/generate_quenched_fraction.m
@@ -1,16 +1,19 @@
 function res =generate_quenched_fraction(fofs,subs,varargin)
 %GENERATE_QUENCHED_FRACTION prepare quenched fraction profiles  binned to
-%host and stellar mass 
+%host and stellar mass
 %   Detailed explanation goes here
 
-global simDisplayName
+%global simDisplayName
 global illUnits
 virType='crit200';
 
-radBinEdges=0.1:0.1:2;
+nboot=1000;
+fffun = @(x)(sum(x)./length(x)); % function for bootstrap
+
+radBinEdges=0:0.2:3.6;
 mainSequenceType='all';
 massThresh=10^9; % threshold for *stellar* mass
-hostBins=[10.7 12:16];
+%hostBins=[10.7 12:16];
 
 %% parse arguments
 i=1;
@@ -25,18 +28,21 @@ while i<=length(varargin)
         case {'radbinedges','radbins'}
             i=i+1;
             radBinEdges=varargin{i};
-             case {'hostbinedges','hostbins'}
+        case {'hostbinedges','hostbins'}
             i=i+1;
             radBinEdges=varargin{i};
-        case 'mask'
+            %         case 'mask'
+            %             i=i+1;
+            %             sampleMask=varargin{i};
+        case 'nboot'
             i=i+1;
-            sampleMask=varargin{i};
+            nboot=varargin{i};
         case 'mainsequence'
             i=i+1;
             mainSequenceType=varargin{i};
-%         case {'massthresh', 'massthreshold'}
-%             i=i+1;
-%             hiMassThresh=varargin{i};
+        case {'massthresh', 'massthreshold'}
+            i=i+1;
+            massThresh=varargin{i};
         otherwise
             error('%s - Illegal argument: %s',current_function().upper,varargin{i});
     end
@@ -48,13 +54,16 @@ end
 subsInfo = illustris.infrastructure.build_sub_fof_connection(subs,fofs);
 % define stellar mass
 galMass= illustris.utils.get_stellar_mass(subs); % stellar mass within 2*rhalf
-% define ssfr 
+% define ssfr
 ssfrBase=double(illustris.utils.calc_ssfr(subs,'base',1e-15));
 
-%% define sample 
+%% define sample
 sampleMask=illustris.infrastructure.generateMask('subs',subs','fofs',fofs,'mass',massThresh);
-cenMask=illustris.infrastructure.generateMask('subs',subs','fofs',fofs,'mass',massThresh,'centrals');
-satMask=illustris.infrastructure.generateMask('subs',subs','fofs',fofs,'mass',massThresh,'sats');
+centrals=illustris.infrastructure.generateMask('subs',subs','fofs',fofs,'mass',massThresh,'centrals');
+sats=illustris.infrastructure.generateMask('subs',subs','fofs',fofs,'mass',massThresh,'sats');
+
+cenMask=centrals(sampleMask);
+satMask=sats(sampleMask);
 
 gmass=galMass(sampleMask);
 ssfr=ssfrBase(sampleMask);
@@ -73,13 +82,20 @@ mainSeq=mk_main_sequence(log10(galMass(msMask)),...
     log10(ssfrBase(msMask)),'bins',8.3:0.15:12.6,...
     'fac',2,'median');
 
-%% extrapolate main sequence relation and define quenched line 
+%% extrapolate main sequence relation and define quenched line
 msX=log10(massThresh):0.05:12.5;
 msY=interp1(mainSeq.msX,mainSeq.msY,msX,'linear','extrap');
-qVal=interp1(mainSeq.msX,mainSeq.msY,log10(gmass),'linear','extrap')-1;% quenched is defined as 1 dex below main sequence 
-qMask=log10(ssfr)<=qVal; % this tells you who is quenched 
+qVal=interp1(mainSeq.msX,mainSeq.msY,log10(gmass),'linear','extrap')-1;% quenched is defined as 1 dex below main sequence
+qMask=log10(ssfr)<=qVal; % this tells you who is quenched
 
-qFracGlobal=sum(qMask)./length(qMask);
+res.quenchedMask=qMask;
+res.mainSeq=mainSeq;
+res.sampleMask=sampleMask;
+
+res.qFracGlobal=sum(qMask)./length(qMask);
+res.qFracGlobalCentrals=sum(qMask(cenMask))./length(qMask);
+res.qFracGlobalSats=sum(qMask(satMask))./length(qMask);
+
 
 %% define virial parameters
 switch lower(virType)
@@ -97,265 +113,144 @@ end
 
 %% calculate distances
 global LBox
-radPosition = findDistance(subs.SubhaloPos(:,sampleMask),fofs.GroupPos(:,subsInfo.hostFof(sampleMask)+1),...
+radPosition = findDistance(double(subs.SubhaloPos(:,sampleMask)),double(fofs.GroupPos(:,subsInfo.hostFof(sampleMask)+1)),...
     LBox,3)./rvir;
 
-% % 
-% % sim='100';
-% % snap=99; %z=0
-% 
-% bp=illustris.set_env(sim);
-% 
-% units; % load general unit structure in cgs.
-% global illUnits
-% global DEFAULT_MATFILE_DIR
+binIndex=discretize(radPosition,radBinEdges);
 
 
+%% build Quenched fraction profiles
 
-%load([DEFAULT_MATFILE_DIR '/fofs_subs_TNG100_z0.mat']);
-subs=illustris.groupcat.loadSubhalos(bp,snap);
-fofs=illustris.groupcat.loadHalos(bp,snap);
+%% split by hostmass
+hmask=false(4,length(mvir));
+hmask(1,:)=log10(mvir)>=10.7 & log10(mvir)<12;
+hmask(2,:)=log10(mvir)>=12 & log10(mvir)<13;
+hmask(3,:)=log10(mvir)>=13 & log10(mvir)<14;
+hmask(4,:)=log10(mvir)>=14 & log10(mvir)<15;
 
-subsInfo = illustris.infrastructure.build_sub_fof_connection(subs,fofs);
-%% set mask
-%%
-
-hostBins=[13 13.5 14 14.5];
-
-galMask=illustris.utils.generateMask('subs',subs','fofs',fofs,'mass',massThresh,'sats');
+res.hostHeader='Host Mass bins: 10.7, 12, 13, 14, 15';
 
 
-ssfrThresh=1e-11;
-%% get data
-%%
-mass=subs.SubhaloMassInRadType(illustris.partTypeNum('stars')+1,galMask).*illUnits.massUnit;
-ssfr=illustris.utils.calc_ssfr(subs);
-ssfr=ssfr(galMask);
-hostMass=fofs.Group_M_Crit200(subs.SubhaloGrNr(galMask)+1).*illUnits.massUnit;
-%% set position in cluster
-%%
-satPos=subs.SubhaloPos(:,galMask);
-hostPos=fofs.GroupPos(:,subs.SubhaloGrNr(galMask)+1);
-hostRv=fofs.Group_R_Crit200(subs.SubhaloGrNr(galMask)+1);
+%% split by stellarmass - 4 groups
+lgMass=log10(gmass);
+smask4=false(4,length(gmass));
+smask4(1,:)=lgMass>=8 & lgMass<9;
+smask4(2,:)=lgMass>=9 & lgMass<10;
+smask4(3,:)=lgMass>=10 & lgMass<11;
+smask4(4,:)=lgMass>=11 ;
+res.byHostStar4.massHeader='stellar mass bins: 8 9 10 11 and above';
 
-satRad=sqrt(sum((satPos-hostPos).^2,1))./hostRv;
-%% some global quenched fractions
-%%
-qmask=ssfr<=ssfrThresh;
-qFracGlobal=sum(qmask)./length(qmask);
+%% split by stellarmass - 2 groups
+smask2=false(2,length(gmass));
+smask2(1,:)=lgMass>=8 & lgMass<10.5;
+smask2(2,:)=lgMass>=10.5;
+res.byHostStar2.massHeader='stellar mass bins: 8 10.5 and above';
 
-%% by host mass:
-%%
-m=hostMass<10.^13 ;
-qm=ssfr(m)<=ssfrThresh;
-qFrac13=sum(qm)./length(qm)
+%% by Host mass only
+byHost.qfrac=zeros(length(radBinEdges)-1,4);
+byHost.bsci=zeros(length(radBinEdges)-1,2,4);
+byHost.xMed=zeros(length(radBinEdges)-1,4);
+byHost.count=zeros(length(radBinEdges)-1,4);
 
-m=hostMass>10.^13  & hostMass<10^13.7;
-qm=ssfr(m)<=ssfrThresh;
-qFrac513=sum(qm)./length(qm)
-
-m=hostMass>10.^13.7 & hostMass<10^14;
-qm=ssfr(m)<=ssfrThresh;
-qFrac14=sum(qm)./length(qm)
-
-m=hostMass>10.^14  & hostMass<10^14.7;
-qm=ssfr(m)<=ssfrThresh;
-qFrac514=sum(qm)./length(qm)
-
-m=hostMass>10.^14.7  & hostMass<10^15;
-qm=ssfr(m)<=ssfrThresh;
-qFrac15=sum(qm)./length(qm)
-%% by stellar mass:
-%%
-m=mass>10.^9  & mass<10^9.7;
-qm=ssfr(m)<=ssfrThresh;
-qFrac59=sum(qm)./length(qm)
-
-m=mass>10.^9.7 & mass<10^10;
-qm=ssfr(m)<=ssfrThresh;
-qFrac10=sum(qm)./length(qm)
-
-m=mass>10.^10  & mass<10^10.7;
-qm=ssfr(m)<=ssfrThresh;
-qFrac510=sum(qm)./length(qm)
-
-m=mass>10.^10.7  & mass<10^11;
-qm=ssfr(m)<=ssfrThresh;
-qFrac11=sum(qm)./length(qm)
-
-m=mass>10.^11.7  & mass<10^12;
-qm=ssfr(m)<=ssfrThresh;
-qFrac511=sum(qm)./length(qm)
-
-
-
-%% find fraction by distance host mass and stellar mass 
-
-dr=0.25;
-rbin=0.25:dr:2.25;
-hmBin=13:0.5:15;
-smBin=9:0.5:12;
-
-satRadbin=ceil(satRad./dr);
-satRadbin(satRadbin>length(rbin))=length(rbin);
-
-qbinT=zeros(length(smBin)-1,length(hmBin),length(rbin));
-nbinT=qbinT;
-for k=1:length(smBin)-1
-    ms=log10(mass)>smBin(k) & log10(mass)<smBin(k+1);
+for j=1:4  %loop over host mass
+    mask1=squeeze(hmask(j,:));
     
-    for j=1:length(hmBin)
+    % quenched fraction of centrals
+    byHost.qFracCen(j)=sum(qMask(cenMask & mask1))./sum(cenMask & mask1);
+    byHost.cenCount(j)=sum(cenMask & mask1);
+    
+    % quenched fraction profiles of satellites
+    mask2=mask1 & satMask;
+    for k=1:length(radBinEdges)-1
+        mask3=mask2 & binIndex==k;
         
-        if j==1
-            mh=log10(hostMass)<hmBin(j);
-        else
-            mh=log10(hostMass)<hmBin(j) & log10(hostMass)>hmBin(j-1);
+        qf=qMask(mask3);
+        byHost.qfrac(k,j)=sum(qf)./length(qf);
+        byHost.xMed(k,j)=median(radPosition(mask3));
+        byHost.count(k,j)=sum(mask3);
+        
+        % generate 95% confidence interval from bootstrap
+        if sum(mask3)>1
+            byHost.bsci(k,:,j)=bootci(nboot,{fffun,qf});
         end
-        
-        for i=1:length(rbin)
-            m=satRadbin==i & mh & ms;
-            qm=ssfr(m)<ssfrThresh;
-            qbinT(k,j,i)=sum(qm);
-            nbinT(k,j,i)=sum(m);
-        end
-        
     end
+    
 end
 
+%% by host Mass and stellar mass - 4 bins
 
 
-
-%% plot quenched fraction by stellar mass and distance 
-
-nbin=squeeze(sum(nbinT,2));
-qbin=squeeze(sum(qbinT,2))./nbin;
-qbin(nbin==0)=0;
-
-
-cmap=brewermap(8,'Set2');
-figure('position',[ 1756 452 805 724])
-%yyaxis left
-
-subplot(6,1,1:4)
-
-h=[];
-for j=1:length(smBin)-1
+for i=1:4  % in stellar mass bins
+    byHostStar4(i).qfrac=zeros(length(radBinEdges)-1,4);
+    byHostStar4(i).bsci=zeros(length(radBinEdges)-1,2,4);
+    byHostStar4(i).xMed=zeros(length(radBinEdges)-1,4);
+    byHostStar4(i).count=zeros(length(radBinEdges)-1,4);
     
-    str=sprintf('$10^{%s}<M_\\mathrm{s}<10^{%s}$',num2str(smBin(j)),num2str(smBin(j+1)));
-    
-    h(j)=plot(rbin,qbin(j,:),'-+','color',cmap(j,:),...
-        'DisplayName',str);
-    hold on
-end
-    
-%hl=legend(h);
-%set(hl,'Interpreter','latex','location','NorthEastOutside')
-    grid
-%xlabelmine('$<r/R_{200}$')
-ylabelmine('quenched fraction')
-titlemine('By steller mass')
-
-
-%figure
-subplot(6,1,5:6)
-%yyaxis left
-h=[];
-for j=1:length(smBin)-1
+    for j=1:4  %loop over host mass
+        mask1=squeeze(hmask(j,:)) & squeeze(smask4(i,:));
+        
+        % quenched fraction of centrals
+        byHostStar4(i).qFracCen(j)=sum(qMask(cenMask & mask1))./sum(cenMask & mask1);
+        byHostStar4(i).cenCount(j)=sum(cenMask & mask1);
+        
+        % quenched fraction profiles of satellites
+        mask2=mask1 & satMask;
+        for k=1:length(radBinEdges)-1
+            mask3=mask2 & binIndex==k;
             
-    str=sprintf('$10^{%s}<M_\\mathrm{s}<10^{%s}$',num2str(smBin(j)),num2str(smBin(j+1)));
-    h(j)=semilogy(rbin,nbin(j,:),'-+','color',cmap(j,:),...
-        'DisplayName',str);
-    hold on
-end
-
-hl=legend(h);
-set(hl,'Interpreter','latex','location','NorthEastOutside','fontsize',10)
-    grid
-xlabelmine('$<r/R_{200}$')
-ylabelmine('$N$ galaxies')
-%titlemine('By steller mass')
-
-
-printout_fig(gcf,['quenchedFraction_stellarMass_' simDisplayName],'v')
-
-%% plot quenched fraction by host mass
-
-nbin=squeeze(sum(nbinT,1));
-qbin=squeeze(sum(qbinT,1))./nbin;
-qbin(nbin==0)=0;
-
-cmap=brewermap(8,'Set1');
-
-figure('position',[ 1756 452 805 724])
-subplot(6,1,1:4)
-h=[];
-for j=1:length(hmBin)
-    if j==1
-    str=sprintf('$M_\\mathrm{h}<10^{%s}$',num2str(hmBin(j)));
-    else
-        str=sprintf('$10^{%s}<M_\\mathrm{h}<10^{%s}$',num2str(hmBin(j-1)),num2str(hmBin(j)));
+            qf=qMask(mask3);
+            byHostStar4(i).qfrac(k,j)=sum(qf)./length(qf);
+            byHostStar4(i).xMed(k,j)=median(radPosition(mask3));
+            byHostStar4(i).count(k,j)=sum(mask3);
+            
+            % generate 95% confidence interval from bootstrap
+            if sum(mask3)>1
+                byHostStar4(i).bsci(k,:,j)=bootci(nboot,{fffun,qf});
+            end
+        end
+        
     end
-    h(j)=plot(rbin,qbin(j,:),'-+','color',cmap(j,:),...
-        'DisplayName',str);
-    hold on
+    
 end
 
-hl=legend(h);
-set(hl,'Interpreter','latex')
-    grid
-%xlabelmine('$<r/R_{200}$')
-ylabelmine('quenched fraction')
-titlemine('By host mass')
-subplot(6,1,5:6)
+%% by host Mass and stellar mass - 4 bins
 
-h=[];
-for j=1:length(hmBin)
-    if j==1
-    str=sprintf('$M_\\mathrm{h}<10^{%s}$',num2str(hmBin(j)));
-    else
-        str=sprintf('$10^{%s}<M_\\mathrm{h}<10^{%s}$',num2str(hmBin(j-1)),num2str(hmBin(j)));
+for i=1:2  % in stellar mass bins
+    byHostStar2(i).qfrac=zeros(length(radBinEdges)-1,4);
+    byHostStar2(i).bsci=zeros(length(radBinEdges)-1,2,4);
+    byHostStar2(i).xMed=zeros(length(radBinEdges)-1,4);
+    byHostStar2(i).count=zeros(length(radBinEdges)-1,4);
+    
+    for j=1:4  %loop over host mass
+        mask1=squeeze(hmask(j,:)) & squeeze(smask2(i,:));
+        
+        % quenched fraction of centrals
+        byHostStar4(i).qFracCen(j)=sum(qMask(cenMask & mask1))./sum(cenMask & mask1);
+        byHostStar4(i).cenCount(j)=sum(cenMask & mask1);
+        
+        % quenched fraction profiles of satellites
+        mask2=mask1 & satMask;
+        for k=1:length(radBinEdges)-1
+            mask3=mask2 & binIndex==k;
+            
+            qf=qMask(mask3);
+            byHostStar4(i).qfrac(k,j)=sum(qf)./length(qf);
+            byHostStar4(i).xMed(k,j)=median(radPosition(mask3));
+            byHostStar4(i).count(k,j)=sum(mask3);
+            
+            % generate 95% confidence interval from bootstrap
+            if sum(mask3)>1
+                byHostStar4(i).bsci(k,:,j)=bootci(nboot,{fffun,qf});
+            end
+        end
+        
     end
-    h(j)=semilogy(rbin,nbin(j,:),'-+','color',cmap(j,:),...
-        'DisplayName',str);
-    hold on
+    
 end
 
-%hl=legend(h);
-%set(hl,'Interpreter','latex')
-    grid
-xlabelmine('$<r/R_{200}$')
-ylabelmine('$N$ galaxies')
+%%
+res.byHost=byHost;
+res.byHostStar2=byHostStar2;
+res.byHostStar4=byHostStar4;
 
-printout_fig(gcf,['quenchedFraction_hostMass_' simDisplayName],'v')
-
-%% plot by host/stellar mass and no radius 
-
-% nbin=squeeze(sum(nbinT,3));
-% qbin=squeeze(sum(qbinT,3))./nbin;
-% qbin(nbin==0)=0;
-% 
-% figure
-% surf(smBin(1:end-1),hmBin,qbin)
-% 
-% figure
-% surf(nbin)
-% 
-% 
-% %% plot mass- ssfr
-% %%
-% figure 
-% cmap=brewermap(256,'*Spectral');
-% scatter(log10(mass),log10(ssfr),3,log10(hostMass))
-% colormap(cmap);
-% xlabelmine('$\log M_\star\,[\mathrm{M_\odot}]$');
-% ylabelmine('$\log \mathrm{sSFR}\,[\mathrm{yr^{-1}}] $');
-% hb=colorbar;
-% grid
-% barTitle(hb,'$\log\,M_\mathrm{host}\,[\mathrm{M_\odot}]$');
-% %% plot pos in host distribution
-% %%
-% [y,x]=hist(log10(satRad),20);
-% bar(x,log10(y));
-% ylabelmine('$\log N$')
-% xlabelmine('$\log r_\mathrm{sat}/R_\mathrm{200,host}$')
-% titlemine('TNG100 satellite position in Host')
+end

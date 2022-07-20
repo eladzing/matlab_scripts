c715408d435e627734739dc54d73a48870a3863d
eladzing <64072621+eladzing@users.noreply.github.com>
2022-07-20 16:22:04 +0300

    Update generate_new_jf_scores_weighted.m
    finalized the new score calculation. The way new scores are calculated is found here, based on the Inspector score. The result is saved in revise_object_tables_with_inspector weights.m

--- IllustrisTNG/jellyfish/generate_new_jf_scores_weighted.m
+++ IllustrisTNG/jellyfish/generate_new_jf_scores_weighted.m
@@ -58,54 +58,54 @@ if readFlag
     
     clsTab=vertcat(clsTabP1,clsTabP2);
     
-    clear clsTabP1 clsTabP2 clsTab0   
-    [idList, ia,~]=unique(clsTab.subject_ids);
-
+    clear clsTabP1 clsTabP2 clsTab0
+    %[idList, ia,~]=unique(clsTab.subject_ids);
+    
 end
 
 %% Identify objects (object index) from CJF that are found in the previous projects.
-
-% TNG50 pilot
-mask50=(objectCJF.sim=="TNG50");
-indCJF50=zeros(height(objectTNG50),1)-1;
-normTNG50=6;
-for j=1:height(objectTNG50)
-    subID=objectTNG50.subfind(j);
-    snp=objectTNG50.snap(j);
-    
-    ind=find(objectCJF.snap==snp & objectCJF.subfind==subID & mask50);
-    if ~isempty(ind)
-        indCJF50(j)=ind;
+if setupFlag
+    % TNG50 pilot
+    mask50=(objectCJF.sim=="TNG50");
+    indCJF50=zeros(height(objectTNG50),1)-1; % tells you the index in CJF of the pilot objects
+    normTNG50=6;
+    for j=1:height(objectTNG50)
+        subID=objectTNG50.subfind(j);
+        snp=objectTNG50.snap(j);
+        
+        ind=find(objectCJF.snap==snp & objectCJF.subfind==subID & mask50);
+        if ~isempty(ind)
+            indCJF50(j)=ind;
+        end
     end
-end
-indMask50=indCJF50>0;
-score50=tally6(indMask50)./normTNG50;  % normalized score
-
-
-
-% TNG100 Yun+2019
-mask100=(objectCJF.sim=="TNG100");
-indCJF100=zeros(height(object100),1)-1; % indices in CJF object table of matched objects
-normTNG100=5;
-for j=1:height(object100)
-    subID=object100.subfind(j);
-    snp=object100.snap(j);
+    indMask50=indCJF50>0;
+    score50=tally6(indMask50);%./normTNG50;  % normalized score
     
-    ind=find(objectCJF.snap==snp & objectCJF.subfind==subID & mask100);
-    if ~isempty(ind)
-        indCJF100(j)=ind;
+    
+    
+    % TNG100 Yun+2019
+    mask100=(objectCJF.sim=="TNG100");
+    indCJF100=zeros(height(object100),1)-1; % indices in CJF object table of matched objects
+    normTNG100=5;
+    for j=1:height(object100)
+        subID=object100.subfind(j);
+        snp=object100.snap(j);
+        
+        ind=find(objectCJF.snap==snp & objectCJF.subfind==subID & mask100);
+        if ~isempty(ind)
+            indCJF100(j)=ind;
+        end
     end
+    
+    indMask100=indCJF100>0;
+    
+    %scoreCJF100=min(objectCJF.score(indCJF100(indMask100))./20,1);
+    score100=double(object100.score(indMask100))./normTNG100;
 end
-
-indMask100=indCJF100>0;
-
-%scoreCJF100=min(objectCJF.score(indCJF100(indMask100))./20,1);
-score100=double(object100.score(indMask100))./normTNG100;
-
 %% generate Inspector weighting
 
 
-% experts 
+% experts
 % identify experts
 iexp(1)=find(iscore.user_name.contains('zinger'));
 iexp(2)=find(iscore.user_name.contains('illepic'));
@@ -113,13 +113,46 @@ iexp(3)=find(iscore.user_name.contains('rohr'));
 iexp(4)=find(iscore.user_name.contains('gandha'));
 iexp(5)=find(iscore.user_name.contains('dnels'));
 expName=iscore.user_name(iexp);
+expWeight=5;
+
+% objectScore-down voting
+
+strikeLimit=3;
+
+downVoteWeight=ones(size(iscoreHigh.score));
+downMask=badScore.downVote>=strikeLimit & badScore.downVoteScore>0.5;
+downVoteWeight(downMask)=0;
+
+upVoteWeight=ones(size(iscoreHigh.score));
+upMask=badScore.upVote>=strikeLimit & badScore.upVoteScore>0.5;
+upVoteWeight(upMask)=0;
+fprintf(' %i strikes \n',strikeLimit);
+fprintf('down votes %i %f \n',sum(downVoteWeight<1),sum(downVoteWeight<1)/6495);
+fprintf('up votes %i %f \n',sum(upVoteWeight<1),sum(upVoteWeight<1)/6495);
+fprintf('both %i %f \n' ,sum(downVoteWeight<1 & upVoteWeight<1),sum(downVoteWeight<1 & upVoteWeight<1)/6495);
 
 
 
+wt_hi=iscoreHigh.score;
+wt_hi(iscoreHigh.ncls<10)=0.5;
+wt_hi_down=wt_hi.*upVoteWeight;
+wt_hi_down_up=wt_hi.*upVoteWeight.*downVoteWeight;
+wt_down_up=upVoteWeight.*downVoteWeight;
 
-%% Run over all objects and generate new score
+wt_hi_exp=wt_hi;
+wt_hi_down_exp=wt_hi_down;
+wt_hi_down_up_exp=wt_hi_down_up;
+wt_down_up_exp=wt_down_up;
+
+wt_hi_exp(iexp)=expWeight;
+wt_hi_down_exp(iexp)=expWeight;
+wt_hi_down_up_exp(iexp)=expWeight;
+wt_down_up_exp(iexp)=expWeight;
+
+%% Run over all random orientatoins  objects and generate new score
 prcStep=10;
 prcLvl=0;
+idList=objectCJF.subject_ids;
 fprintf('Running over %i objects \n \n',length(idList))
 for i=1:length(idList)
     
@@ -130,26 +163,120 @@ for i=1:length(idList)
         prcLvl=prcLvl+prcStep;
     end
     
-    % find all classifications     
+    % find all classifications
     inds=find(clsTab.subject_ids==idList(i));
-    
-  
-    
-    Ncls=length(inds); 
+        
+    Ncls=length(inds);
     objTab.clsNum(i)=Ncls; % total number of classifications
     
     votes=clsTab.isJelly(inds);
-    baseScore=sum(votes)./length(votes);
+    newScore.baseScore(i)=sum(votes)./length(votes);
     
     
-    
-    %% generate new scores 
-    
-    %identify if it is in a previous study
+    % identify Inspectors of this object
+    unames=clsTab.user_name(inds);
     
     
+    %% identify if it is in a previous study
+    wt0=0;
+    score0=0;
+    i100=find(i==indCJF100);
+    if ~isempty(i100)
+        wt0=normTNG100.*expWeight;
+        score0=double(object100.score(i100).*expWeight);
+    end
+    
+    i50=find(i==indCJF50);
+    
+    if ~isempty(i50)
+        wt0=normTNG50.*expWeight;
+        score0=double(max(tally6(i50)).*expWeight);
+    end
+    
+    %% generate new scores
+    % find their scores
+    [~,ii]=ismember(unames,iscore.user_name);
+        
+    newScore.score_hi(i)=(score0+sum(wt_hi(ii).*votes))/(wt0+sum(wt_hi(ii)));
+    newScore.score_hi_down(i)=(score0+sum(wt_hi_down(ii).*votes))/(wt0+sum(wt_hi_down(ii)));
+    newScore.score_hi_down_up(i)=(score0+sum(wt_hi_down_up(ii).*votes))/(wt0+sum(wt_hi_down_up(ii)));
+    
+    newScore.score_down_up(i)=(score0+sum(wt_down_up(ii).*votes))/(wt0+sum(wt_down_up(ii)));
+    
+    newScore.score_hi_exp(i)=(score0+sum(wt_hi_exp(ii).*votes))/(wt0+sum(wt_hi_exp(ii)));
+    newScore.score_hi_down_exp(i)=(score0+sum(wt_hi_down_exp(ii).*votes))/(wt0+sum(wt_hi_down_exp(ii)));
+    newScore.score_hi_down_up_exp(i)=(score0+sum(wt_hi_down_up_exp(ii).*votes))/(wt0+sum(wt_hi_down_up_exp(ii)));
+    
+    newScore.score_down_up_exp(i)=(score0+sum(wt_down_up_exp(ii).*votes))/(wt0+sum(wt_down_up_exp(ii)));
+    
+    %% 
+    newScore.weight0(i)=wt0;
+    newScore.ncls0(i)=wt0./expWeight;
+    newScore.weight(i)=wt0+sum(wt_hi_down_up_exp(ii));
+    newScore.ncls(i)=sum(wt_hi_down_up_exp(ii)~=0);
+    newScore.nexp(i)=sum(wt_hi_down_up_exp(ii)==expWeight);    
+    newScore.nclsOriginal(i)=Ncls;
 end
 
+
+%% repeat for the preferred direction 
+
+prcStep=10;
+prcLvl=0;
+idList=objectTablePref.subject_ids;
+fprintf('Running over %i objects \n \n',length(idList))
+for i=1:length(idList)
+    
+    % advancement ticker
+    prc=round(i./length(idList).*100);
+    if prc>=prcLvl
+        fprintf('completed %i %% of objects \n',prc);
+        prcLvl=prcLvl+prcStep;
+    end
+    
+    % find all classifications
+    inds=find(clsTab.subject_ids==idList(i));
+        
+    Ncls=length(inds);
+    %objTab.clsNum(i)=Ncls; % total number of classifications
+    
+    votes=clsTab.isJelly(inds);
+    newScorePref.baseScore(i)=sum(votes)./length(votes);
+    
+    
+    % identify Inspectors of this object
+    unames=clsTab.user_name(inds);
+    
+    
+    %% identify if it is in a previous study
+    wt0=0;
+    score0=0;
+
+    
+    %% generate new scores
+    % find their scores
+    [~,ii]=ismember(unames,iscore.user_name);
+        
+    newScorePref.score_hi(i)=(score0+sum(wt_hi(ii).*votes))/(wt0+sum(wt_hi(ii)));
+    newScorePref.score_hi_down(i)=(score0+sum(wt_hi_down(ii).*votes))/(wt0+sum(wt_hi_down(ii)));
+    newScorePref.score_hi_down_up(i)=(score0+sum(wt_hi_down_up(ii).*votes))/(wt0+sum(wt_hi_down_up(ii)));
+    
+    newScorePref.score_down_up(i)=(score0+sum(wt_down_up(ii).*votes))/(wt0+sum(wt_down_up(ii)));
+    
+    newScorePref.score_hi_exp(i)=(score0+sum(wt_hi_exp(ii).*votes))/(wt0+sum(wt_hi_exp(ii)));
+    newScorePref.score_hi_down_exp(i)=(score0+sum(wt_hi_down_exp(ii).*votes))/(wt0+sum(wt_hi_down_exp(ii)));
+    newScorePref.score_hi_down_up_exp(i)=(score0+sum(wt_hi_down_up_exp(ii).*votes))/(wt0+sum(wt_hi_down_up_exp(ii)));
+    
+    newScorePref.score_down_up_exp(i)=(score0+sum(wt_down_up_exp(ii).*votes))/(wt0+sum(wt_down_up_exp(ii)));
+    
+    %% 
+%     newScorePref.weight0(i)=wt0;
+%     newScorePref.ncls0(i)=wt0./expWeight;
+    newScorePref.weight(i)=wt0+sum(wt_hi_down_up_exp(ii));
+    newScorePref.ncls(i)=sum(wt_hi_down_up_exp(ii)~=0);
+    newScorePref.nexp(i)=sum(wt_hi_down_up_exp(ii)==expWeight);    
+    newScorePref.nclsOriginal(i)=Ncls;
+end
 
 
 

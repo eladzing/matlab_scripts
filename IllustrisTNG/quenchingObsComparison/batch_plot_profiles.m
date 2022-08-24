f745da8a1be812d0bdc2db5b68965e632442b155
eladzing <64072621+eladzing@users.noreply.github.com>
2022-08-24 15:11:13 +0300

    Update batch_plot_profiles.m
    plotted different things

--- IllustrisTNG/quenchingObsComparison/batch_plot_profiles.m
+++ IllustrisTNG/quenchingObsComparison/batch_plot_profiles.m
@@ -2,19 +2,22 @@
 
 
 %% radius 
+plotRadProf(hprofs50,'Gal','TNG50',1,'xl',[0 2.1],'print')
+plotRadProf(hprofs50,'CGMall','TNG50',1,'xl',[0 2.1],'print')
+plotRadProf(hprofs50,'Gal','TNG50',3,'xl',[0 2.1],'print')
+plotRadProf(hprofs50,'CGMall','TNG50',3,'xl',[0 2.1],'print')
 
-plotRadProf(hprofs50,'CGMall','TNG50',1,'print')
-plotRadProf(hprofs100,'Gal','TNG100',1,'print')
-plotRadProf(hprofs100,'CGMall','TNG100',1,'print')
+plotRadProf(hprofs100,'Gal','TNG100',1,'xl',[0 2.1])
+plotRadProf(hprofs100,'CGMall','TNG100',1,'xl',[0 2.1])
 close all
 
 
-%ssfr 
-plotsSFRProf(hprofs50,'Gal','TNG50',1,'print')
-plotsSFRProf(hprofs50,'CGMall','TNG50',1,'print')
-plotsSFRProf(hprofs100,'Gal','TNG100',1,'print')
-plotsSFRProf(hprofs100,'CGMall','TNG100',1,'print')
+%% ssfr 
+plotsSFRProf(hprofs50,'Gal','TNG50',1)
+plotsSFRProf(hprofs50,'CGMall','TNG50',1)
+plotsSFRProf(hprofs100,'Gal','TNG100',1)
+plotsSFRProf(hprofs100,'CGMall','TNG100',1)
 close all
 
-plotRadProf_massN(hprofs50,'Gal','TNG50',1,'print')
-plotRadProf_massN(hprofs50,'CGMall','TNG50',1,'print')
+plotRadProf_massN(hprofs50,'Gal','TNG50',1)
+plotRadProf_massN(hprofs50,'CGMall','TNG50',1)

2d1b6024eb3aa395e5877fd44daeed382468e0da
eladzing <64072621+eladzing@users.noreply.github.com>
2023-02-21 18:11:13 +0200

    Update addCellRadius.m
    set the default behavior to calculate in simulation units., but left an option to get physical units instead

--- IllustrisTNG/+illustris/+utils/addCellRadius.m
+++ IllustrisTNG/+illustris/+utils/addCellRadius.m
@@ -1,12 +1,24 @@
-function gasStruct =  addCellRadius( gasStruct )
+function gasStruct =  addCellRadius( gasStruct,unitType )
 %ADDCEELLRADIUS calculate the 'spherical' cell radius returns in adjusted
 %units
-%   assume cell is a sphere and find the radius based on mass and density. 
-%    mass and density are in adjusted (no /h)  physical units. 
+%   assume cell is a sphere and find the radius based on mass and density.
+%    mass and density are in simulation units (comoving /h) by default but 
+%    can be set to physical units (no /h) .
 
+if ~exist('unitType','var')
+    unitType='simulation'; % comoving /h - same as in the structure
+end
 global illUnits
 
-gasStruct.CellRadius= (3/(4*pi).* ( gasStruct.Masses.*illUnits.massUnit)./ (gasStruct.Density.*illUnits.densityUnit)).^(1/3);
+switch lower(unitType)
+    case 'simulation'
+        gasStruct.CellRadius= (3/(4*pi).* ( gasStruct.Masses)./ (gasStruct.Density)).^(1/3);
+    case 'physical'
+        gasStruct.CellRadius= (3/(4*pi).* ( gasStruct.Masses.*illUnits.massUnit)./ (gasStruct.Density.*illUnits.densityUnit)).^(1/3);
+    otherwise
+        error('%s - unknown unit type: %s',current_function().upper,unitType);
+end
+
 
 end
 

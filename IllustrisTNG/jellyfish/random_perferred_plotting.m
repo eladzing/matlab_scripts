2ce71f97611151b35ab601326827d5f71baa2b8e
eladzing <64072621+eladzing@users.noreply.github.com>
2023-03-15 17:27:18 +0200

    Update random_perferred_plotting.m
    some improved plots for the viewing angle comparison

--- IllustrisTNG/jellyfish/random_perferred_plotting.m
+++ IllustrisTNG/jellyfish/random_perferred_plotting.m
@@ -49,6 +49,23 @@ set(gca,'FontSize',axFont,'YMinorTick','on','YScale','log');
 fname='cjf_rand_pref_weight_hist';
 if printFlag; printout_fig(gcf,fname,'nopdf','v','printoutdir',outdir); end
 
+
+
+%% difference histogram 
+bnn=-1.025:0.05:1.025;
+myFigure;
+% histogram(objectTableComp.scoreRawPref-objectTableComp.scoreRawRand,-20.5:1:20.5)
+% hold on
+histogram(objectTableComp.scoreWeightedPref-objectTableComp.scoreWeightedRand,bnn)%,-17.5:0.5:17.5)
+grid
+set(gca,'yscale','log')
+xlim([-1.025 1.025])
+%legend('Raw','weighted','Interpreter','latex','fontsize',14);
+xlabelmine('Score Preferred - Random');
+ylabelmine('No. of objects');
+
+
+
 %% 2D histogram 
 scR=objectTableComp.scoreWeightedRand;
 scP=objectTableComp.scoreWeightedPref;
@@ -56,6 +73,16 @@ ng=height(objectTableComp);
 
 [bird, binsize, xl,yl]= histogram2d(scR,scP,ones(size(scR)),'len',21);
 bbird=squeeze(bird(:,:,1));
+
+% [bird2, binsize2, xl2,yl2]= histogram2d(scR,scP-scR,ones(size(scR)),'len',[43 21],...
+%     'xlim',[0 1],'yylim',[-1 1]);
+% bbird2=squeeze(bird2(:,:,1));
+% 
+% [bird3, binsize3, xl3,yl3]= histogram2d(scP,scP-scR,ones(size(scR)),'len',[42 21],...
+%     'xlim',[0 1],'yylim',[-1 1]);
+% bbird3=squeeze(bird3(:,:,1));
+
+
 
 q1=sum(scP>=0.8 & scR<0.8)/ng*100;
 q4=sum(scP<0.8 & scR>=0.8)/ng*100;
@@ -90,6 +117,66 @@ barTitle(hb,'log \%','fontsize',labFont)
 
 fname='cjf_rand_pref_weight_2dhist';
 if printFlag; printout_fig(gcf,fname,'nopdf','v','printoutdir',outdir); end
+
+
+%% 
+% %% plot 2D histogram of difference 
+% hf=myFigure;
+% imagesc(xl2,yl2,log10(bbird2./sum(sum(bbird2)).*100))
+% hold on
+% %plot([-0.5 21]./20,[-0.5 21]./20,'--k','linewidth',1.5)
+% %plot([15.5 15.5]./20,[-0.5 21]./20,':k','linewidth',1.5)
+% %plot([-0.5 21]./20,[15.5 15.5]./20,':k','linewidth',1.5)
+% xlabelmine('Random',labFont);
+% ylabelmine('Preferred-Random',labFont);
+% %colormap(brewermap(256,'Reds'))
+% colormap(flipud(magma(256)));
+% %caxis(log10([0.01 20])); 
+% % text(0.05,0.95,[num2str(q1,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% % text(0.8,0.95,[num2str(q2,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% % text(0.05,0.7,[num2str(q3,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% % text(0.8,0.7,[num2str(q4,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% 
+% set(gca,'ydir','normal','fontsize',axFont)
+% hb=colorbar;
+% barTitle(hb,'log \%','fontsize',labFont)
+% %titlemine('Weighted Score');
+% 
+% fname='cjf_rand_pref_weight_2dhist_diffR';
+% if printFlag; printout_fig(gcf,fname,'nopdf','v','printoutdir',outdir); end
+% 
+% %% plot 2D histogram of difference 
+% hf=myFigure;
+% imagesc(xl3,yl3,log10(bbird3./sum(sum(bbird3)).*100))
+% hold on
+% %plot([-0.5 21]./20,[-0.5 21]./20,'--k','linewidth',1.5)
+% %plot([15.5 15.5]./20,[-0.5 21]./20,':k','linewidth',1.5)
+% %plot([-0.5 21]./20,[15.5 15.5]./20,':k','linewidth',1.5)
+% xlabelmine('Preferred',labFont);
+% ylabelmine('Preferred-Random',labFont);
+% %colormap(brewermap(256,'Reds'))
+% colormap(flipud(magma(256)));
+% %caxis(log10([0.01 20])); 
+% % text(0.05,0.95,[num2str(q1,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% % text(0.8,0.95,[num2str(q2,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% % text(0.05,0.7,[num2str(q3,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% % text(0.8,0.7,[num2str(q4,2) '\%'],'Interpreter','latex',...
+% %     'fontsize',22)
+% 
+% set(gca,'ydir','normal','fontsize',axFont)
+% hb=colorbar;
+% barTitle(hb,'log \%','fontsize',labFont)
+% %titlemine('Weighted Score');
+% 
+% fname='cjf_rand_pref_weight_2dhist_diffP';
+% if printFlag; printout_fig(gcf,fname,'nopdf','v','printoutdir',outdir); end
 
 %% Some stats 
 

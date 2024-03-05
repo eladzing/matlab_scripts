814c4d1fa77ca23bc32520e50bddd67eb0e15975
eladzing <64072621+eladzing@users.noreply.github.com>
2024-03-05 14:16:03 +0200

    Update mkmapGas.m

--- IllustrisTNG/+illustris/+plots/mkmapGas.m
+++ IllustrisTNG/+illustris/+plots/mkmapGas.m
@@ -469,7 +469,7 @@ if typeFlag
             logFlag=false;
             %weight=cubeStr.weights; % cube of mass in each uniform grid cell
             bartag='$\rho v_r\,[\mathrm{M_\odot/kpc^3\,km/sec}]$';
-            slTypeDef='avg';
+            slTypeDef='sum';
             map = brewermap(256,'*RdBu');
             
             %             %find radial velocity component
